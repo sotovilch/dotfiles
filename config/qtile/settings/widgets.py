760ffa1163ed5322921e754a06f7f0526670fed0
Gerardo Soto <gersoto3110.developer@gmail.com>
2024-10-04 13:12:09 -0300

    Feaure qtile: Add widget to indicate caps/num lock

--- config/qtile/settings/widgets.py
+++ config/qtile/settings/widgets.py
@@ -1,9 +1,40 @@
+import subprocess
+from glob import glob
+from typing import Literal
+
 from libqtile import widget
 
 widget_icon_defaults = {
     "padding": 1,
     "fontsize": 20,
 }
+
+
+class CapsNumLockIndicator(widget.CapsNumLockIndicator):
+    def __init__(self, **config):
+        super().__init__(**config)
+
+    def get_indicator(self, lock: Literal["capslock", "numlock"], on: str, off: str):
+        """Return on/off indicators for CapsLock or NumLock."""
+        try:
+            output = self.call_process(
+                ["cat"] + glob(f"/sys/class/leds/*{lock}/brightness")
+            )
+        except subprocess.CalledProcessError as err:
+            output = err.output
+            return off
+
+        if "1" in output:
+            return on
+        else:
+            return off
+
+    def poll(self):
+        """Poll content for the text box."""
+        capslockIndicator = self.get_indicator("capslock", " 󰬈 ", "")
+        numlockIndicator = self.get_indicator("numlock", " 󰎤 ", "")
+
+        return f"{capslockIndicator}{numlockIndicator}"
 
 
 def init_primary_widgets(groups: str):
@@ -26,6 +57,7 @@ def init_primary_widgets(groups: str):
         widget.Clock(
             format="%Y-%m-%d %a %H:%M", background="#5e81ac", foreground="#ffffff"
         ),
+        CapsNumLockIndicator(**widget_icon_defaults),
         widget.QuickExit(),
     ]
 
