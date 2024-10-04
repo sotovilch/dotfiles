da2923a29a11b8954657abbe4b18fa768efcde43
Gerardo Soto <gersoto3110.developer@gmail.com>
2024-10-04 13:06:25 -0300

    Refactor qtile: Move key bindings

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -33,88 +33,21 @@ from settings.layout import init_layouts, init_floating_layout
 from settings.wayland import init_wayland_input_rules
 from settings.groups import init_groups
 from settings.screens import init_screens
+from settings.keys import init_key_bindings
 from utils.connected_monitors import get_connected_monitors
 from utils.lazy_functions import go_to_group, move_to_group
 
 GROUPS_PER_MONITOR = ["uiop", "7890", "1234"]
 
-mod = "mod4"
+# Modifier key: more info https://docs.qtile.org/en/latest/manual/config/keys.html#modifiers
+# To see a list of modifier names and their matching keys, use the xmodmap command in your terminal.
+mod = "lock"
 terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key(
-        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
-    ),
-    Key(
-        [mod, "shift"],
-        "l",
-        lazy.layout.shuffle_right(),
-        desc="Move window to the right",
-    ),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
-    ),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key(
-        [mod],
-        "f",
-        lazy.window.toggle_fullscreen(),
-        desc="Toggle fullscreen on the focused window",
-    ),
-    Key(
-        [mod],
-        "t",
-        lazy.window.toggle_floating(),
-        desc="Toggle floating on the focused window",
-    ),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    *init_key_bindings(mod),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
-
-# Add key bindings to switch VTs in Wayland.
-# We can't check qtile.core.name in default config as it is loaded before qtile is started
-# We therefore defer the check until the key binding is run by using .when(func=...)
-for vt in range(1, 8):
-    keys.append(
-        Key(
-            ["control", "mod1"],
-            f"f{vt}",
-            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
-            desc=f"Switch to VT{vt}",
-        )
-    )
 
 connected_monitors = get_connected_monitors(max_monitors=len(GROUPS_PER_MONITOR))
 
---
+++ config/qtile/settings/keys.py
@@ -0,0 +1,111 @@
+from libqtile import qtile
+from libqtile.config import Key, KeyChord
+from libqtile.lazy import lazy
+
+
+def __init_key_bindings_to_monad_tall(mod_key):
+    return [
+        # Basics
+        Key([mod_key], "q", lazy.window.kill(), desc="Kill focused window"),
+        Key(
+            [mod_key],
+            "f",
+            lazy.window.toggle_fullscreen(),
+            desc="Toggle fullscreen on the focused window",
+        ),
+        Key(
+            [mod_key],
+            "t",
+            lazy.window.toggle_floating(),
+            desc="Toggle floating on the focused window",
+        ),
+        # Move focus
+        Key([mod_key], "h", lazy.layout.left(), desc="Move focus to left"),
+        Key([mod_key], "l", lazy.layout.right(), desc="Move focus to right"),
+        Key([mod_key], "j", lazy.layout.down(), desc="Move focus down"),
+        Key([mod_key], "k", lazy.layout.up(), desc="Move focus up"),
+        # Move windows in MonadTall layout.
+        Key(
+            [mod_key, "shift"],
+            "h",
+            lazy.layout.swap_left(),
+            desc="Move window to the left",
+        ),
+        Key(
+            [mod_key, "shift"],
+            "l",
+            lazy.layout.swap_right(),
+            desc="Move window to the right",
+        ),
+        Key(
+            [mod_key, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"
+        ),
+        Key([mod_key, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+        Key([mod_key, "shift"], "f", lazy.layout.flip(), desc="Switch main pane"),
+        # Resize window
+        KeyChord(
+            [mod_key],
+            "m",
+            [
+                Key([mod_key], "n", lazy.layout.normalize(), desc="Reset sizes"),
+                Key([mod_key], "g", lazy.layout.grow(), desc="Grow window"),
+                Key([mod_key], "s", lazy.layout.shrink(), desc="Shrink window"),
+                Key([mod_key], "r", lazy.layout.reset(), desc="Reset window size"),
+                Key([mod_key], "m", lazy.layout.maximize(), desc="Maximize window"),
+                Key([mod_key], "q", lazy.ungrab_chord()),
+            ],
+            mode=True,
+            name="Window resize",
+        ),
+    ]
+
+
+def __init_key_bindings_to_switch_vts_in_wayland():
+    keys = []
+
+    # Add key bindings to switch VTs in Wayland.
+    # We can't check qtile.core.name in default config as it is loaded before qtile is started
+    # We therefore defer the check until the key binding is run by using .when(func=...)
+    for vt in range(1, 8):
+        keys.append(
+            Key(
+                ["control", "mod1"],
+                f"f{vt}",
+                lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+                desc=f"Switch to VT{vt}",
+            )
+        )
+
+    return keys
+
+
+def __init_key_bindings_to_apps(mod_key):
+    return [
+        KeyChord(
+            [mod_key],
+            "a",
+            [
+                Key([], "t", lazy.spawn("alacritty")),
+                Key([], "f", lazy.spawn("firefox")),
+                Key([], "v", lazy.spawn("vlc")),
+                KeyChord(
+                    [],
+                    "q",
+                    [
+                        Key([], "r", lazy.reload_config(), desc="Reload the config"),
+                        Key([], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+                    ],
+                    name="Qtile opt.",
+                ),
+            ],
+            name="Apps",
+        )
+    ]
+
+
+def init_key_bindings(mod_key):
+    return [
+        *__init_key_bindings_to_monad_tall(mod_key),
+        *__init_key_bindings_to_switch_vts_in_wayland(),
+        *__init_key_bindings_to_apps(mod_key),
+    ]
