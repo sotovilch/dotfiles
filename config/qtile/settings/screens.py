b0e3348b5549bdae6c5baeffdb4c35dc475c5478
Gerardo Soto <gersoto3110.developer@gmail.com>
2024-10-23 11:32:51 -0300

    Refactor qtile: Screen - Add default options

--- config/qtile/settings/screens.py
+++ config/qtile/settings/screens.py
@@ -3,6 +3,12 @@ from libqtile.config import Screen
 
 from settings.widgets import init_primary_widgets, init_secondary_widgets
 
+default_screen_options = {
+    "wallpaper": "wallpaper/nissan-skyline.jpg",
+    "wallpaper_mode": "fill",
+    "x11_drag_polling_rate": None,
+}
+
 
 def init_screens(connected_monitors: int, groups_per_monitor: list[str]):
     screens = [
@@ -10,13 +16,8 @@ def init_screens(connected_monitors: int, groups_per_monitor: list[str]):
             top=bar.Bar(
                 init_primary_widgets(groups=groups_per_monitor[0]),
                 24,
-                # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-                # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
             ),
-            # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-            # By default we handle these events delayed to already improve performance, however your system might still be struggling
-            # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-            # x11_drag_polling_rate = 60,
+            **default_screen_options,
         ),
     ]
 
@@ -27,7 +28,8 @@ def init_screens(connected_monitors: int, groups_per_monitor: list[str]):
                     top=bar.Bar(
                         init_secondary_widgets(groups=groups_per_monitor[index_screen]),
                         24,
-                    )
+                    ),
+                    **default_screen_options,
                 )
             )
 
