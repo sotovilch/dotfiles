1060d286d69ba441a05e20861109a7cbf210222b
Gerardo Soto <gersoto3110.developer@gmail.com>
2024-12-03 12:47:27 -0300

    Fix qtile: set environment

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -2,3 +2,6 @@
 
 # Notification daemon
 dunst &
+
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=qtile &
