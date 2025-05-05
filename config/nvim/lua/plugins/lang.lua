7bcd68c36503130b2089597ad5ebdae1f0156e8a
Gerardo Soto <gersoto3110.developer@gmail.com>
2025-05-05 09:22:14 -0300

    Feature nvim: Add tailwind

--- config/nvim/lua/plugins/lang.lua
+++ config/nvim/lua/plugins/lang.lua
@@ -1,14 +1,6 @@
 return {
   -- Python
   {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.auto_brackets = opts.auto_brackets or {}
-      table.insert(opts.auto_brackets, "python")
-    end,
-  },
-
-  {
     "linux-cultist/venv-selector.nvim",
     opts = function(_, opts)
       if LazyVim.has("nvim-dap-python") then
@@ -209,4 +201,51 @@ return {
       table.insert(opts.ensure_installed, "js-debug-adapter")
     end,
   },
+
+  --Tailwind
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          -- exclude a filetype from the default_config
+          filetypes_exclude = { "markdown" },
+          -- add additional filetypes to the default_config
+          filetypes_include = {},
+          -- to fully override the default_config, change the below
+          -- filetypes = {}
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = LazyVim.lsp.get_raw_config("tailwindcss")
+          opts.filetypes = opts.filetypes or {}
+
+          -- Add default filetypes
+          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+          -- Remove excluded filetypes
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, opts.filetypes)
+
+          -- Additional settings for Phoenix projects
+          opts.settings = {
+            tailwindCSS = {
+              includeLanguages = {
+                elixir = "html-eex",
+                eelixir = "html-eex",
+                heex = "html-eex",
+              },
+            },
+          }
+
+          -- Add additional filetypes
+          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+        end,
+      },
+    },
+  },
+  { "roobert/tailwindcss-colorizer-cmp.nvim", opts = {} },
 }
