308a6f3d0d5ffb73696beac0a94cd29b98208a58
Gerardo Soto <gersoto3110.developer@gmail.com>
2025-05-05 09:22:40 -0300

    Feature nvim: Add black formatter

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -4,4 +4,19 @@ return {
     "williamboman/mason.nvim",
     opts = { ensure_installed = { "prettier" } },
   },
+  -- black
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "black")
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "black" },
+      },
+    },
+  },
 }
