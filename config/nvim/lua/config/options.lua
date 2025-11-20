1d590726cb722242fdd1bd0057788f31a572b6fb
Gerardo Soto <gersoto3110.developer@gmail.com>
2025-11-20 11:33:27 -0300

    Update nvim config

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,10 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.lazyvim_python_lsp = "pyright"
-
--- Enable the option to require a Prettier config file
--- If no prettier config file is found, the formatter will not be used
-vim.g.lazyvim_prettier_needs_config = false
-
 vim.opt.wrap = true
